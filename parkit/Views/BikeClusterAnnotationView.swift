75c44661c3528d4df5ac70fddc90f56a691188bd
Arthur Péligry <arthur.peligry@gmail.com>
2019-05-22 14:42:42 +0200

    updating cgrect for map

---
+++ parkit/Views/BikeClusterAnnotationView.swift
@@ -0,0 +1,50 @@
+//
+//  ClusterAnnotationView.swift
+//  parkit
+//
+//  Created by Arthur Péligry on 22/05/2019.
+//  Copyright © 2019 Arthur Péligry. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import Cluster
+
+class BikeClusterAnnotationView: ClusterAnnotationView {
+    
+    override func configure() {
+        super.configure()
+        
+        guard let annotation = annotation as? ClusterAnnotation else { return }
+        let count = annotation.annotations.count
+        let diameter = radius(for: count) * 2
+        let bgColor = color(for: count)
+        
+        self.frame.size = CGSize(width: diameter, height: diameter)
+        self.layer.cornerRadius = self.frame.width / 2
+        self.layer.masksToBounds = true
+        self.layer.backgroundColor = bgColor
+        self.layer.borderColor = UIColor.white.cgColor
+        self.layer.borderWidth = 1.5
+    }
+    
+    func color(for count: Int) -> CGColor {
+        if count < 5 {
+            return UIColor.red.cgColor
+        } else if count < 10 {
+            return UIColor.yellow.cgColor
+        } else {
+            return UIColor.green.cgColor
+        }
+    }
+    
+    func radius(for count: Int) -> CGFloat {
+        if count < 5 {
+            return 12
+        } else if count < 10 {
+            return 16
+        } else {
+            return 20
+        }
+    }
+}
